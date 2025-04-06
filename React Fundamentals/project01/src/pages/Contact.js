6393cd96353503e4ea962c1fb42947692bd2f587
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-06 19:36:36 +0530

    Use The UseState for store data value

--- React Fundamentals/project01/src/pages/Contact.js
+++ React Fundamentals/project01/src/pages/Contact.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import "../component/contact.css";
 
 export default function Contact() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount (count+1);
+  };
+  const decrement = () => {
+    setCount (count-1);
+  };
   return (
     <div>
       <h1>This is Contact Page</h1>
+      <h1>The count is {count}</h1>
+      <button onClick={decrement} className="btn">- </button>
+      <button onClick={increment} className="btn">+</button>
     </div>
   );
 }
