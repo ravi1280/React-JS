bd9c003d507334d74cb53bfeb626bf1e7e937468
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-06 12:02:16 +0530

    Create Component and Call

--- React Fundamentals/project01/src/App.js
+++ React Fundamentals/project01/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import TestOne from './component/TestOne';
 
 function App() {
   return (
     <div className="App">
     <h1>This Is Mt React App</h1>
+    <TestOne/>
+    <TestOne/>
+    <TestOne/>
     </div>
   );
 }
---
+++ React Fundamentals/project01/src/component/TestOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestOne() {
+  return (
+    <div>
+      <h1>This is my first React Component</h1>
+    </div>
+  );
+}
