0e8107e3d00b38477b53e096285cdc7febde7e1d
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-10 21:14:51 +0530

    Update2.01

--- React-LandingPage/src/component/Review.jsx
+++ React-LandingPage/src/component/Review.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import img1 from "../assets/img/review1.jpg"
-import img2 from "../assets/img/review2.jpg"
-import img3 from "../assets/img/review3.jpg"
+import img1 from "../assets/img/Review1.png"
+import img2 from "../assets/img/Review2.png"
+import img3 from "../assets/img/Review3.png"
 import ReviewCard from '../layout/ReviewCard'
 
 
@@ -14,21 +14,21 @@ const Review = () => {
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 py-5">
         <ReviewCard
           img={img1}
-          title="John D., Digital Artist"
+          title="John D . ( Digital Artist )"
           contend={
             "I’ve been into NFTs for a while, but avatarX took the experience to a whole new level. The platform is easy to use, and I love how simple it is to buy and sell exclusive digital creations. Highly recommend it to anyone looking to explore the NFT world!"
           }
         />
         <ReviewCard
           img={img2}
-          title="Sarah W., NFT Collector"
+          title="Sarah W . ( NFT Collector )"
           contend={
             "I found some of the most rare and valuable digital collectibles on avatarX. The variety of NFTs available, from avatars to music tracks, is unmatched. It's a one-stop shop for every NFT enthusiast!"
           }
         />
         <ReviewCard
           img={img3}
-          title="Mike L., Digital Entrepreneur"
+          title="Mike L . ( Digital Entrepreneur )"
           contend={
             "The team at avatarX is fantastic! I had a few questions about the purchasing process, and their customer service was incredibly helpful. The whole buying and selling process is smooth, and I feel secure with every transaction."
           }
--- React-LandingPage/src/layout/ReviewCard.jsx
+++ React-LandingPage/src/layout/ReviewCard.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const ReviewCard = ({img , title,contend}) => {
   return (
-    <div className='shadow-[0_3px_10px_rgb(0,0,0,0.2)]  p-5 rounded-2xl transition-transform transform hover:scale-95 duration-300 bg-zinc-800 '>
-        <img className='rounded-2xl' src={img} alt="img" />
+    <div className='shadow-[0_3px_10px_rgb(0,0,0,0.2)]   p-5 rounded-2xl transition-transform transform hover:scale-95 duration-300 bg-zinc-800 '>
+        <div className="flex justify-center mb-4">
+        <img className='rounded-2xl h-70 ' src={img} alt="img" />
+      </div>
         <h3 className='text-lg- font-semibold text-center mt-3 '>{title}</h3>
         <p className='font-light text-sm mt-5 '>{contend}</p>
       
