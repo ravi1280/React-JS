8015daa4408f88a02707cf2f9b144d392de98085
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-10 16:14:04 +0530

    Framer Motion Add

--- React-LandingPage/src/layout/AboutCard.jsx
+++ React-LandingPage/src/layout/AboutCard.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 const AboutCard = ({icon,title,contend}) => {
   return (
-    <div className=' flex flex-col  items-center gap-2 text-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-5 rounded-2xl  hover:text-white transition-transform transform hover:bg-emerald-500  hover:-translete-y-2 hover:scale-105  duration-300 border hover:border-0'>
-        <span className=' text-lg- font-semibold'>{icon}</span>
-        <h3 className='text-sm font-light'>{title}</h3>
-        <p>{contend}</p>
-      
-    </div>
-  )
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className=" flex flex-col  items-center gap-2 text-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-5 rounded-2xl  hover:text-white transition-transform transform hover:bg-emerald-500  hover:-translete-y-2 hover:scale-105  duration-300 border hover:border-0"
+    >
+      <span className=" text-lg- font-semibold">{icon}</span>
+      <h3 className="text-sm font-light">{title}</h3>
+      <p>{contend}</p>
+    </motion.div>
+  );
 }
 
 export default AboutCard
