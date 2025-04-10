c07d515671d1818e8b7d8afe4c7a179c43f5eb5e
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-10 12:06:49 +0530

    UPDATE GALLERY

--- React-LandingPage/src/component/Product.jsx
+++ React-LandingPage/src/component/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import img1 from "../assets/img/prod1.jpg"
-import img2 from "../assets/img/prod2.jpg"
-import img3 from "../assets/img/prod3.jpg"
+import img1 from "../assets/img/prod1.png"
+import img2 from "../assets/img/prod2.png"
+import img3 from "../assets/img/Character.png"
 import img4 from "../assets/img/prod4.jpg"
 import img5 from "../assets/img/prod5.jpg"
 import img6 from "../assets/img/prod6.jpg"
@@ -9,19 +9,56 @@ import ProductCard from '../layout/ProductCard'
 
 const Product = () => {
   return (
-    <div className='min-h-screen  px-5 md:px-14 py-8'>
-      <h1 className='text-4xl text-emerald-500  text-center font-bold py-5'>Our Products !</h1>
-      <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-14 mt-2'>
-      <ProductCard img={img1} title={"POS System"}/>
-      <ProductCard img={img2} title={"POS System"}/>
-      <ProductCard img={img3} title={"POS System"}/>
-      <ProductCard img={img4} title={"POS System"}/>
-      <ProductCard img={img5} title={"POS System"}/>
-      <ProductCard img={img6} title={"POS System"}/>
+    <div className="min-h-screen  px-5 md:px-14 py-8">
+      <h1 className="text-4xl text-emerald-500  text-center font-bold py-5">
+        Gallery
+      </h1>
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-14 mt-2">
+        <ProductCard
+          img={img1}
+          title={"The Digital Future"}
+          contend={
+            "Explore our exclusive digital avatars, each designed to capture a futuristic and personalized identity."
+          }
+        />
+        <ProductCard
+          img={img2}
+          title={"Creator's Art"}
+          contend={
+            "Showcasing the finest creations by talented artists, available exclusively as NFTs. Own a piece of digital art history."
+          }
+        />
+        <ProductCard
+          img={img3}
+          title={"Digital Real Estate"}
+          contend={
+            " Invest in virtual lands and properties. Buy, sell, or develop in the metaverse with exclusive virtual assets."
+          }
+        />
+        <ProductCard
+          img={img3}
+          title={"Music for the Digital Age"}
+          contend={
+            "Unlock limited-edition music albums and tracks as NFTs, giving you ownership of exclusive sound experiences."
+          }
+        />
+        <ProductCard
+          img={img3}
+          title={"Rare Collectibles"}
+          contend={
+            "Get your hands on limited-edition NFTs, collectible items that will forever remain one of a kind in the digital world."
+          }
+        />
+        <ProductCard
+          img={img3}
+          title={"Collaboration Projects"}
+          contend={
+            " Experience collaboration at its best—where multiple creators join forces to create a unique NFT project. Own a part of their collective creativity."
+          }
+        />
       </div>
-     
     </div>
-  )
+  );
 }
 
 export default Product
--- React-LandingPage/src/layout/ProductCard.jsx
+++ React-LandingPage/src/layout/ProductCard.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
-const ProductCard = ({img,title}) => {
+const ProductCard = ({img,title ,contend}) => {
   return (
-    <div className=" bg-zinc-800 p-3.5  rounded-lg text-center space-y-2 group hover:cursor-pointer hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-300 ease-in-out ">
-      <img src={img} alt="img" className="rounded-2xl" />
+    <div className=" bg-zinc-800 p-3.5  rounded-lg text-center space-y-2 group hover:cursor-pointer hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition duration-300 ease-in-out  ">
+      <div className='flex  items-center justify-center'>
+      <img src={img} alt="img" className="rounded-2xl h-70 " /></div>
       <h3 className=" text-white text-lg-font-semibold ">{title}</h3>
       <div className="flex  items-center justify-center">
-        <p className="text-sm  font-light group-hover:opacity-0 transition-opacity  duration-300">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur quae
-          deserunt omnis quisquam dolore autem itaque necessitatibus odit
-          recusandae saepe beatae nisi fugiat, quidem non.
+        <p className="text-sm  font-light group-hover:opacity-0 transition-opacity  duration-300 p-5">
+          {contend}
         </p>
         <button className="opacity-0 group-hover:opacity-100 bg-emerald-500 text-white px-4 py-2 rounded-lg font-semibold transition-opacity duration-300 absolute ">
           View Now
