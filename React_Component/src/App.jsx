4521e11b18f0a15ab1a99e11d2ef4535b0e122ca
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-04-23 00:34:16 +0530

    Crad UI

--- React_Component/src/App.jsx
+++ React_Component/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ExampleText from "./Component/ExampleText";
+import Card from "./Component/TilCard";
 
 function App() {
 
 
   return (
     <>
-      <ExampleText />
+      <Card />
     </>
   );
 }
